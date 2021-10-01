ea987ae2a3ce8ba54444e5bb1127fe06986b8031
Vishwa Pinnawala <45973257+vishwapinnawala@users.noreply.github.com>
2021-10-01 20:20:50 +0530

    Add files via upload

---
+++ ConsoleApplication8/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Circle : Shapes  // Derived class (child) 
+    {
+        public new void draw()
+        {
+            Console.WriteLine("This Draws a Circle");
+        }
+        public new void erase()
+        {
+            Console.WriteLine("This Erases a Circle");
+        }
+    }
+}
